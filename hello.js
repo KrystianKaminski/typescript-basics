1ca9f99374eada53bd280c76bc61ec3721d36c7c
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-12-12 09:55:03 +0100

    First program with TypeScript

---
+++ hello.js
@@ -0,0 +1,10 @@
+var HelloWorld = /** @class */ (function () {
+    function HelloWorld() {
+    }
+    HelloWorld.main = function () {
+        console.log('Hello word');
+        return 0;
+    };
+    return HelloWorld;
+}());
+HelloWorld.main();
---
+++ hello.ts
@@ -0,0 +1,8 @@
+class HelloWorld {
+    public static main(): number {
+        console.log('Hello word')
+        return 0
+    }
+}
+
+HelloWorld.main()
